fde4afe0b999ac86c25780e0f1e5eb1077584b2a
elibretti <joshuaprpich@gmail.com>
2019-01-02 17:31:42 -0500

    initial import

---
+++ lib/session.rb
@@ -0,0 +1,32 @@
+require 'json'
+require 'byebug'
+class Session
+  # find the cookie for this app
+  # deserialize the cookie into a hash
+  attr_reader :cookies_hash
+  def initialize(req)
+    cookies = req.cookies["_rails_lite_app"]
+    if cookies
+      @cookie_hash = JSON.parse(cookies)
+    else
+      @cookie_hash = { }
+    end
+    # debugger
+  end
+  
+  def [](key)
+    @cookie_hash[key]
+  end
+
+  def []=(key, val)
+    @cookie_hash[key] = val 
+  end
+
+  # serialize the hash into json and save in a cookie
+  # add to the responses cookies
+  def store_session(res)
+    cookie = { path: '/', value: @cookie_hash.to_json }
+    res.set_cookie("_rails_lite_app", cookie )
+  end
+end
+
